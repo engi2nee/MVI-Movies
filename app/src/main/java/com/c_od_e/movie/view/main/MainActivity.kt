69cd49802b4e77a7c4acc52445d66a31e73fcabe
Engi2nee <engi2nee@gmail.com>
2020-02-22 23:10:55 +0300

    Created required fragments

---
+++ app/src/main/java/com/c_od_e/movie/view/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.c_od_e.movie.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c_od_e.movie.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
